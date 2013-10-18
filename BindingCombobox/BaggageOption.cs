293795cc9ed62d015ef4353234d746afb1cf93f0
DW <dw7@outlook.com.au>
2013-10-18 13:22:00 +1000

    xxx

---
+++ BindingCombobox/BaggageOption.cs
@@ -0,0 +1,25 @@
+﻿namespace FlightCentre.TudFaregateProvider.Common
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BaggageOption
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal Tax { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int AllowedPieces { get; set; }
+        public string Key { get; set; }
+
+        public decimal MaxWeight { get; set; }
+
+        //HACK: I couldn't get the baggage combobox to display the description of the selected item. Instead it just displays the string representation (ToString())
+        public override string ToString()
+        {
+            return Description;
+        }
+    }
+}

27440a7e2131d8e2dc1f0fa388ab30ca7f7c3e31
DW <dw7@outlook.com.au>
2013-10-18 13:24:28 +1000

    xxx

---
+++ BindingCombobox/MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FlightCentre.TudFaregateProvider.Common;
+
+namespace BindingCombobox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public PassengerBaggageInfo passengerBaggageInfo { get; set; }
+        public ObservableCollection<BaggageOption> MyBaggageOptions = createBaggageOptions();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            passengerBaggageInfo = new PassengerBaggageInfo
+            {
+                SelectedBaggageCode = "3a",
+                BaggageOptions = createBaggageOptions()
+            };
+        }
+
+        private static List<PassengerInfo> createPassengers()
+        {
+            List<PassengerInfo> travelers = new List<PassengerInfo>();
+            travelers.Add(new PassengerInfo(DateTime.Now.AddDays(30))
+            {
+                
+                TravellerFirstName = "firstn",
+                TravellerSurname = "lastn",
+                TravellerTitle = "Miss",
+                Key = Guid.NewGuid().ToString()
+            });
+            travelers.Add(new PassengerInfo(DateTime.Now.AddDays(30))
+            {
+                TravellerFirstName = "childn",
+                TravellerSurname = "lastn",
+                TravellerTitle = "Dr",
+                Key = Guid.NewGuid().ToString()
+            });
+            return travelers;
+        }
+
+        private static ObservableCollection<BaggageOption> createBaggageOptions()
+        {
+            ObservableCollection<BaggageOption> baggageOptions = new ObservableCollection<BaggageOption>
+            {
+                new BaggageOption
+                {
+                    AllowedPieces = 1,
+                    Code = "1a",
+                    Description = "First Item",
+                    ID = 1,
+                    Key = "1",
+                    MaxWeight = 10,
+                    Name = "Item 1",
+                    Price = 10,
+                    Tax = 1
+                },
+                new BaggageOption
+                {
+                    AllowedPieces = 1,
+                    Code = "2a",
+                    Description = "Second Item",
+                    ID = 2,
+                    Key = "2",
+                    MaxWeight = 20,
+                    Name = "Item 2",
+                    Price = 20,
+                    Tax = 1
+                },
+                new BaggageOption
+                {
+                    AllowedPieces = 1,
+                    Code = "3a",
+                    Description = "Third Item",
+                    ID = 3,
+                    Key = "3",
+                    MaxWeight = 30,
+                    Name = "Item 3",
+                    Price = 30,
+                    Tax = 1
+                }
+            };
+            return baggageOptions;
+        }
+
+        private void Bind1_Click(object sender, RoutedEventArgs e)
+        {
+            Combo1.DataContext = passengerBaggageInfo;
+            TestDataGrid.ItemsSource = new List<PassengerBaggageInfo> {passengerBaggageInfo};
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Combo1.SelectedItem != null)
+                //MessageBox.Show(((BaggageOption) Combo1.SelectedItem).Code);
+                MessageBox.Show(passengerBaggageInfo.SelectedBaggageCode);
+        }
+
+        private void DataGridComboSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox combo = (ComboBox)sender;
+
+            BaggageOption option = combo.SelectedItem as BaggageOption;
+            PassengerBaggageInfo currentPassengerInfo = ((PassengerBaggageInfo)TestDataGrid.SelectedItem);
+
+            string code = option.Code;
+            decimal price = option.Price;
+            decimal tax = option.Tax;
+            decimal weight = option.MaxWeight;
+
+            currentPassengerInfo.SelectedBaggageCode = code;
+            currentPassengerInfo.Price = price;
+            currentPassengerInfo.Tax = tax;
+            currentPassengerInfo.MaxWeight = weight;
+        }
+    }
+}
---
+++ BindingCombobox/PassengerBindingInfo.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+
+namespace FlightCentre.TudFaregateProvider.Common
+{
+    public class PassengerBaggageInfo
+    {
+        public ObservableCollection<BaggageOption> BaggageOptions { get; set; }
+        public string Sector { get; set; }
+        public string SelectedBaggageCode { get; set; }
+        /// <summary>
+        /// Price including tax
+        /// </summary>
+        public decimal Price { get; set; }
+
+        public decimal Tax { get; set; }
+        public decimal MaxWeight { get; set; }
+        public int SegmentIndex { get; set; }
+
+        public string TravellerName { get; set; }
+    }
+
+    public class PassengerInfo : IDataErrorInfo
+    {
+        private string dateOfBirth;
+        private string selectedCompanionName;
+        private int selectedCompanionIndex;
+
+        public int TravellerIndex { get; set; }
+        public string TravellerTitle { get; set; }
+        public string TravellerFirstName { get; set; }
+        public string TravellerSurname { get; set; }
+        
+        /// <summary>
+        /// Date of Birth
+        /// </summary>
+      
+        public string Key { get; set; }
+      
+       
+        public List<string> Titles { get; set; }
+        public PassengerInfo(DateTime finalTravelDate)
+        {
+            FinalTravelDate = finalTravelDate;
+        }
+        /// <summary>
+        /// Used for validation purposes
+        /// </summary>
+        protected DateTime? FinalTravelDate { get; set; }
+
+        private List<PassengerBaggageInfo> baggageInfos = new List<PassengerBaggageInfo>();
+        public List<PassengerBaggageInfo> BaggageInfos
+        {
+            get { return baggageInfos; }
+            set { baggageInfos = value; }
+        }
+
+     
+     
+        public string this[string columnName]
+        {
+            get
+            {
+                return validate(columnName);
+            }
+        }
+
+        private string validate(string columnName)
+        {
+            StringBuilder result = new StringBuilder();
+            bool checkAllProperties = string.IsNullOrWhiteSpace(columnName);
+
+            if (checkAllProperties || (columnName.Contains("SelectedCompanion")))
+            {
+                
+            }
+
+           
+            if (checkAllProperties || columnName == "TravellerTitle")
+            {
+                if (string.IsNullOrWhiteSpace(TravellerTitle))
+                    result.AppendLine("Title must be provided for all passengers");
+            }
+
+            return result.ToString();
+        }
+
+        public string Error
+        {
+            get { return validate(null); }
+        }
+
+        public string TravellerName
+        {
+            get { return string.Format("{0} {1}", this.TravellerFirstName, this.TravellerSurname); }
+        }
+    }
+}
